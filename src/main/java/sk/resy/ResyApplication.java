0d1715fec0f6f69d1d8a48b8935d3c4e50cd44a0
juhasz <ext-ladislav.juhasz@here.com>
2022-03-07 14:14:44 +0100

    lambda priklady

--- src/main/java/sk/resy/ResyApplication.java
+++ src/main/java/sk/resy/ResyApplication.java
@@ -3,6 +3,11 @@ package sk.resy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
 @SpringBootApplication
 public class ResyApplication {
 
@@ -10,5 +15,70 @@ public class ResyApplication {
         SpringApplication.run(ResyApplication.class, args);
     }
 
+    // comment
+
+
+    // lambda expressions
+
+
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+
+    public void run() {
+
+        var c = 10;
+
+        var x = urobNiecoA(10, a -> a * c); // 100
+        var y = urobNiecoA(10, a -> a - 5 ); // 5
+        var z = urobNiecoA(10, a -> a % 2 ); // 5
+        var b = urobNiecoA(10, new Nasobitel());
+
+
+        numbers.forEach(a -> System.out.println(a));
+        numbers.forEach(System.out::println);
+
+        for (Integer a : numbers) {
+            System.out.println(a);
+        }
+
+        List<Integer> res = numbers // 1, 5, 2
+                .stream()
+                .map(a -> a * 11) // vsetky sa vynasobia 10-tkou -> 11, 66, 22
+                .filter(a -> a % 2 == 0)
+              .filter(ResyApplication::filterEven)
+                .toList();
+
+        List<Integer> res2 = new ArrayList<>();
+        for (Integer n : numbers) {
+            var temp = n * 11;
+            if (temp % 2 == 0) res2.add(temp);
+        }
+
+
+    }
+
+    public static boolean filterEven(int x ) {
+        return x % 2 == 0;
+    }
+
+
+
+    public Integer urobNiecoB(int x) {
+        return x * 10;
+    }
+
+    public Integer urobNiecoA(int x, Function<Integer, Integer> f) {
+        return f.apply(x);
+    }
+
+    private class Nasobitel implements Function<Integer, Integer> {
+        @Override public Integer apply(Integer integer ) { return integer * 10;}
+    }
+
 }
 
+
+// @FunctionalInterface
+// public interface Foo {
+//     String method();
+// }
